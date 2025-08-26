3016d3c61d4cfb26f04ed1d0da4a50ebc14d3a23
Eddy <51528872+0x777A@users.noreply.github.com>
2025-08-26 20:08:04 +0800

    upgrade anchor to 0.31.1 (#23)

--- programs/launch-cpi/src/lib.rs
+++ programs/launch-cpi/src/lib.rs
@@ -8,14 +8,14 @@ pub use states::*;
 
 use anchor_lang::prelude::*;
 #[cfg(feature = "devnet")]
-declare_id!("LanD8FpTBBvzZFXjTxsAoipkFsxPUCDB4qAqKxYDiNP");
+declare_id!("DRay6fNdQ5J82H7xV6uq2aV3mNrUZ1J4PgSKsWgptcm6");
 #[cfg(not(feature = "devnet"))]
 declare_id!("LanMV9sAd7wArD4vJFi2qDdfnVhFxYSUg6eADduJ3uj");
 
 pub mod admin {
     use super::{pubkey, Pubkey};
     #[cfg(feature = "devnet")]
-    pub const ID: Pubkey = pubkey!("adMCyoCgfkg7bQiJ9aBJ59H3BXLY3r5LNLfPpQfMzBe");
+    pub const ID: Pubkey = pubkey!("DRayqG9RXYi8WHgWEmRQGrUWRWbhjYWYkCRJDd6JBBak");
     #[cfg(not(feature = "devnet"))]
     pub const ID: Pubkey = pubkey!("GThUX1Atko4tqhN2NaiTazWSeFWMuiUvfFnyJyUghFMJ");
 }
@@ -31,7 +31,7 @@ pub mod openbook_program {
 pub mod amm_program {
     use super::{pubkey, Pubkey};
     #[cfg(feature = "devnet")]
-    pub const ID: Pubkey = pubkey!("HWy1jotHpo6UqeQxx49dpYYdQB8wj9Qk9MdxwjLvDHB8");
+    pub const ID: Pubkey = pubkey!("DRaya7Kj3aMWQSy19kSjvmuwq9docCHofyP9kanQGaav");
     #[cfg(not(feature = "devnet"))]
     pub const ID: Pubkey = pubkey!("675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8");
 }
@@ -39,7 +39,7 @@ pub mod amm_program {
 pub mod cpswap_program {
     use super::{pubkey, Pubkey};
     #[cfg(feature = "devnet")]
-    pub const ID: Pubkey = pubkey!("CPMDWBwJDtYax9qW7AyRuVC19Cc4L4Vcy4n2BHAbHkCW");
+    pub const ID: Pubkey = pubkey!("DRaycpLY18LhpbydsBWbVJtxpNv9oXPgjRSfpF2bWpYb");
     #[cfg(not(feature = "devnet"))]
     pub const ID: Pubkey = pubkey!("CPMMoo8L3F4NbTegBCKVNunggL7H1ZpdTHKxQB5qKP1C");
 }
@@ -47,7 +47,7 @@ pub mod cpswap_program {
 pub mod lock_program {
     use super::{pubkey, Pubkey};
     #[cfg(feature = "devnet")]
-    pub const ID: Pubkey = pubkey!("DLockwT7X7sxtLmGH9g5kmfcjaBtncdbUmi738m5bvQC");
+    pub const ID: Pubkey = pubkey!("DRay25Usp3YJAi7beckgpGUC7mGJ2cR1AVPxhYfwVCUX");
     #[cfg(not(feature = "devnet"))]
     pub const ID: Pubkey = pubkey!("LockrWmn6K5twhz3y9w1dQERbmgSaRkfnTeTKbpofwE");
 }
@@ -72,6 +72,37 @@ pub mod raydium_launchpad {
         Ok(())
     }
 
+    /// Initializes a new trading pool
+    /// # Arguments
+    ///
+    /// * `ctx` - The context of accounts containing pool and token information
+    ///
+    pub fn initialize_v2(
+        ctx: Context<InitializeV2>,
+        base_mint_param: MintParams,
+        curve_param: CurveParams,
+        vesting_param: VestingParams,
+        amm_fee_on: AmmCreatorFeeOn,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    /// Initializes a new trading pool with base token belongs to spl-token-2022,
+    /// pool created by this instruction must be migrated to cpswap after fundraising ends, i.e., curve_param.migrate_type = 1
+    /// # Arguments
+    ///
+    /// * `ctx` - The context of accounts containing pool and token information
+    ///
+    pub fn initialize_with_token_2022(
+        ctx: Context<InitializeWithToken2022>,
+        base_mint_param: MintParams,
+        curve_param: CurveParams,
+        vesting_param: VestingParams,
+        amm_fee_on: AmmCreatorFeeOn,
+        transfer_fee_extension_param: Option<TransferFeeExtensionParams>,
+    ) -> Result<()> {
+        Ok(())
+    }
     /// Use the given amount of quote tokens to purchase base tokens.
     /// # Arguments
     ///
@@ -184,6 +215,15 @@ pub mod raydium_launchpad {
         Ok(())
     }
 
+    /// Claim the fee from the exclusive platform fee vault.
+    /// # Arguments
+    ///
+    /// * `ctx` - The context of accounts
+    ///
+    pub fn claim_platform_fee_from_vault(ctx: Context<ClaimPlatformFeeFromVault>) -> Result<()> {
+        Ok(())
+    }
+
     /// Update platform config
     /// # Arguments
     ///
@@ -194,6 +234,42 @@ pub mod raydium_launchpad {
         ctx: Context<UpdatePlatformConfig>,
         param: PlatformConfigParam,
     ) -> Result<()> {
+        Ok(())
+    }
+
+    /// Update platform launch param
+    /// # Arguments
+    ///
+    /// * `ctx` - The context of accounts
+    /// * `bonding_curve_param` - Parameter to update
+    ///
+    pub fn update_platform_curve_param(
+        ctx: Context<UpdatePlatformCurveParam>,
+        index: u8,
+        bonding_curve_param: BondingCurveParam,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    /// Remove platform launch param
+    /// # Arguments
+    ///
+    /// * `ctx` - The context of accounts
+    /// * `index` - The index of the curve param to remove
+    ///
+    pub fn remove_platform_curve_param(
+        ctx: Context<RemovePlatformCurveParam>,
+        index: u8,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    /// Claim the fee from the exclusive creator fee vault.
+    /// # Arguments
+    ///
+    /// * `ctx` - The context of accounts
+    ///
+    pub fn claim_creator_fee(ctx: Context<ClaimCreatorFee>) -> Result<()> {
         Ok(())
     }
 }
